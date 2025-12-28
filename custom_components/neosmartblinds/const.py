8882f7552797801f49726fe8865b5c3fb86c9785
MrToast99 <31112122+MrToast99@users.noreply.github.com>
2025-12-28 14:03:33 -0700

    Update const.py
    Room Control, Code Cleanup

--- custom_components/neosmartblinds/const.py
+++ custom_components/neosmartblinds/const.py
@@ -2,30 +2,24 @@
 
 DOMAIN = "neosmartblinds"
 
-# Endpoints from HAR file
 API_BASE_URL = "https://api.neosmartblinds.com"
 API_TOKEN_URL = f"{API_BASE_URL}/oauth/token"
 API_LOCATION_URL = f"{API_BASE_URL}/location" 
 
-# --- CONFIRMED FROM HAR FILE ---
 API_COMMAND_URL = f"{API_BASE_URL}/esp32/multi-transmit"
 API_SCHEDULE_UPDATE_URL = f"{API_BASE_URL}/location/{{uuid}}/schedules/{{schedule_id}}"
-# --- END CONFIRMED ---
 
-# --- CONFIRMED COMMANDS FROM HAR FILE ---
 CMD_UP = "up"
 CMD_DOWN = "dn"
 CMD_STOP = "sp"
 CMD_FAV = "i1"
 CMD_FAV2 = "i2"
-CMD_GP = "gp" # <-- ADD THIS
+CMD_GP = "gp"
 
-# --- TDBU / TILT COMMANDS (from local PDF) ---
+# --- TDBU / TILT COMMANDS ---
 CMD_TDBU_MIDDLE_UP = "u4"
 CMD_TDBU_MIDDLE_DOWN = "d4"
 CMD_TDBU_LOWER_UP = "u2"
 CMD_TDBU_LOWER_DOWN = "d2"
-# --- END ADDED ---
 
-# Client ID from HAR file
 CLIENT_ID = "mobile"
